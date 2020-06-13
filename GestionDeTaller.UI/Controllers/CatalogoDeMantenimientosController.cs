8decd3e55d5cb2001520f550cc23b6efc7817649
Anthony Ortega <metal4lifelml@outlook.com>
2020-06-13 11:05:50 -0600

    Se añadio el catalogo de mantenimientos y la vista listar

---
+++ GestionDeTaller.UI/Controllers/CatalogoDeMantenimientosController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GestionDeTaller.BL;
+using GestionDeTaller.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionDeTaller.UI.Controllers
+{
+    public class CatalogoDeMantenimientosController : Controller
+    {
+        private IRepositorioDelTaller RepositorioDelTaller;
+        public CatalogoDeMantenimientosController(IRepositorioDelTaller repositorioDeLibros)
+        {
+            RepositorioDelTaller = repositorioDeLibros;
+        }
+
+        // GET: CatalogoDeMantenimientos
+        public ActionResult Listar(Articulo articulo)
+        {
+            List<Mantenimientos> laListaDeMantenimientos;
+            laListaDeMantenimientos = RepositorioDelTaller.ObtenerLosMantenimientos(articulo);
+            return View(laListaDeMantenimientos);
+        }
+
+        // GET: CatalogoDeMantenimientos/Details/5
+        public ActionResult Detalles(int id)
+        {
+            return View();
+        }
+
+        // GET: CatalogoDeMantenimientos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CatalogoDeMantenimientos/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Listar));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CatalogoDeMantenimientos/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CatalogoDeMantenimientos/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Listar));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CatalogoDeMantenimientos/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CatalogoDeMantenimientos/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Listar));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
